44708a1de76820ab946f040ae6cc0539e15f98ca
まさたか <shinonome419@icloud.com>
2023-01-25 15:30:16 +0900

    add: TextFieldと必要なパラメータを追加し、titleとheightを挿入

--- lib/widgets/custom_form_widget.dart
+++ lib/widgets/custom_form_widget.dart
@@ -7,6 +7,14 @@ class CustomFormWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    //titleとheightはコンストラクタで都度、入れ替えるのでconstは付かない
+    return TextField(
+      decoration: InputDecoration(
+        hintText: title,
+        border: InputBorder.none,
+        //高さを変更するためのパラメータ。下の高さしか必要なかったので、bottomのみ指定
+        contentPadding: EdgeInsets.only(bottom: height),
+      ),
+    );
   }
 }
