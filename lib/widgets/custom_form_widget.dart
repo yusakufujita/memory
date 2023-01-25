1cdeb17b5cbff035ff9fa485fc86de70b1e43ebd
まさたか <shinonome419@icloud.com>
2023-01-25 15:26:11 +0900

    add: custom_form_widgetファイルを作成。titleとheightのコンストラクタを追加

---
+++ lib/widgets/custom_form_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomFormWidget extends StatelessWidget {
+  final String title;
+  final double height;
+  CustomFormWidget(this.title, this.height);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
